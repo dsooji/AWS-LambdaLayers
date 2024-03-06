afb60badd18125820fd3ca463aa69cbac36823db
dsooji - github <dsooji@yahoo.in>
2024-03-06 11:20:48 +0530

    Updated Code4

--- LambdaLayer.tf
+++ LambdaLayer.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region  = "ap-south-1"
-  profile = "Deepen"
-}
-
 resource "aws_lambda_layer_version" "lambda_layer" {
   filename   = "${path.module}/Modules/requests.zip"
   layer_name = "requestmodulelayer-tf"
