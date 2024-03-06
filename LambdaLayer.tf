b3be19bb9f66f0f17d4a92c1b4860bcb9d810b0b
dsooji - github <dsooji@yahoo.in>
2024-03-06 17:21:33 +0530

    Updated Code

--- LambdaLayer.tf
+++ LambdaLayer.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename   = "${path.module}/Modules/requests.zip"
-  layer_name = "requestmodulelayer-tf"
-
+  s3_bucket         = "deepen-s3-bucket"
+  s3_key            = "dependencies.zip"
+  layer_name        = "requestmodulelayer-tf"
   compatible_runtimes = ["python3.12"]
 }
